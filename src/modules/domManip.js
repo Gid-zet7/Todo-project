228241c1e5ebc90e8d243ad9410cdf637f21af24
Gideon Adonteng <gadonteng002@st.ug.edu.gh>
2023-10-10 23:52:58 +0000

    Create Task UI
    The module is now able to add a task to a specific project and load that project's tasks onto the screen

--- src/modules/domManip.js
+++ src/modules/domManip.js
@@ -1,9 +1,11 @@
 import LocalStorage from "./LocalStorage";
 import Project from "./Project";
+import Task from "./Task";
 
 const domManip = () => {
   function loadHome() {
     loadProjects();
+    loadProjectContent("Inbox");
   }
   function createProjectUI(projectName) {
     const projectContainer = document.querySelector(".project-list");
@@ -64,10 +66,105 @@ const domManip = () => {
     createProjectInput.value = "";
   }
 
+  function loadProjectContent(projectName) {
+    const projectContainer = document.querySelector(".project__content");
+    projectContainer.innerHTML = `
+      <h1 id="project-name">${projectName}</h1>
+      <div class="tasks-list" id="tasks-list"></div>`;
+
+    if (projectName !== "Today Tasks" && projectName !== "Upcoming Tasks") {
+      projectContainer.innerHTML += `
+      <button class="btn-add-task" id="button-add-task">
+        Add Task
+      </button>`;
+    }
+
+    loadTasks(projectName);
+  }
+
+  function createTaskUI(title, description, dueDate, priority) {
+    const taskList = document.querySelector(".tasks-list");
+    // const text = title
+    const formatTitle = title.split(" ").join("");
+    taskList.innerHTML += `
+    <div class="task-ui" id=${formatTitle}>
+      <p><input type="radio" name="radio"></input>${title}</p>
+      <p>${description}</p>
+      <p>${dueDate}</p>
+      <p>${priority}</p>
+    </div>
+    `;
+  }
+
+  function loadTasks(projectName) {
+    const projects = LocalStorage().getTodo().projects;
+    const getProject = (projectName) => {
+      return projects.find((project) => project.name === projectName);
+    };
+    console.log(getProject(projectName));
+    getProject(projectName).tasks.forEach((task) =>
+      createTaskUI(task.name, task.description, task.dueDate, task.priority)
+    );
+    if (projectName !== "Today Tasks" && projectName !== "Upcoming Tasks") {
+      initTaskAddBtn();
+    }
+  }
+
+  function addTask() {
+    const projectName = document.getElementById("project-name").textContent;
+    const titleInput = document.getElementById("title");
+    const descriptionInput = document.getElementById("description");
+    const dueDateInput = document.getElementById("dueDate");
+    const priorityInput = document.getElementById("priority");
+
+    const taskTitle = titleInput.value;
+    const description = descriptionInput.value;
+    const dueDate = dueDateInput.value;
+    const priority = priorityInput.value;
+
+    if (taskTitle === "") {
+      alert("Title cannot be empty");
+      return;
+    }
+
+    const localStore = LocalStorage();
+    const getProject = (projectName) => {
+      return localStore
+        .getTodo()
+        .projects.find((project) => project.name === projectName);
+    };
+
+    const projectTasks = getProject(projectName).tasks;
+    const exists = (taskName) => {
+      return projectTasks.some((task) => task.name === taskName);
+    };
+
+    if (exists(taskTitle)) {
+      alert("Task already exists");
+      // Create a function to clear all input fields
+      return;
+    }
+
+    localStore.addTask(
+      projectName,
+      Task(taskTitle, description, dueDate, priority)
+    );
+
+    createTaskUI(taskTitle, description, dueDate, priority);
+
+    // Create a function to clear all input fields
+  }
+
   function initAddProjectBtn() {
     const create = document.getElementById("create");
 
     create.addEventListener("click", addProject);
+  }
+
+  function initTaskAddBtn() {
+    const submit = document.getElementById("submit");
+
+    submit.addEventListener("click", addTask);
   }
 
   return { loadHome };
